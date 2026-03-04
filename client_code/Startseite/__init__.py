bfff56936302948eb309796b0fe7b039b7ae3645
karan.oezcelik@student.htl-rankweil.at <karan.oezcelik@student.htl-rankweil.at>
2026-03-04 10:42:48 +0000

    Created OIP.webp, ServerModule1, Startseite, Startseite.Medizin and 2 more; Edited settings; Deleted Form1

---
+++ client_code/Startseite/__init__.py
@@ -0,0 +1,26 @@
+from ._anvil_designer import StartseiteTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Startseite(StartseiteTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  @handle("drop_down_1", "change")
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    if self.drop_down_1.selected_value == "Startseite":
+      open_form('Startseite')
+    elif self.drop_down_1.selected_value == "Medizin":
+      open_form('Startseite.Medizin')
+    elif self.drop_down_1.selected_value == "Organisation":
+      open_form('Startseite.Organisation')
+    elif self.drop_down_1.selected_value == "Statistik":
+      open_form('Startseite.Statistik')
+      
